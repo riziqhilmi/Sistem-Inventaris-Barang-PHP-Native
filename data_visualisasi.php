75294ddf40103195d2f5cc15993b9b8e4ee5dff9
E31230007_RizkaYulianaSafitri_Gol A <158010453+RizkaYuliana27@users.noreply.github.com>
2024-11-27 11:34:06 +0700

    grafikbrngmsuk

--- data_visualisasi.php
+++ data_visualisasi.php
@@ -7,7 +7,6 @@ if (!isset($_SESSION['user_id'])) {
 }
 ?>
 
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -32,89 +31,141 @@ if (!isset($_SESSION['user_id'])) {
                 <div class="container">
                     <h1 class="mb-4">Data Visualisasi</h1>
                     <div class="container">
-    <div class="row">
-        <!-- Card 1 -->
-        <div class="col-lg-4 col-md-6 mb-3">
-            <div class="card small-card">
-                <img src="img/vector_visual.jpg" class="card-img-top" alt="jpg">
-                <div class="card-body">
-                    <h4 class="card-title">Data visualisasi guru</h4>
-                    
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-        <!-- Card 2 -->
-        <div class="col-lg-4 col-md-6 mb-3">
-            <div class="card small-card">
-                <img src="img/vector_visual.jpg" class="card-img-top" alt="jpg">
-                <div class="card-body">
-                    <h4 class="card-title">Data visualisasi siswa</h4>
-                    
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-        <!-- Card 3 -->
-        <div class="col-lg-4 col-md-6 mb-3">
-            <div class="card small-card">
-                <img src="img/vector_visual.jpg" class="card-img-top" alt="jpg">
-                <div class="card-body">
-                    <h4 class="card-title">Data visualisasi barang</h4>
-                    
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <!-- Card 4 -->
-        <div class="col-lg-4 col-md-6 mb-3">
-            <div class="card small-card">
-                <img src="img/vector_visual.jpg" class="card-img-top" alt="jpg">
-                <div class="card-body">
-                    <h4 class="card-title">Data visualisasi barang masuk</h4>
-                   
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-        <!-- Card 5 -->
-        <div class="col-lg-4 col-md-6 mb-3">
-            <div class="card small-card">
-                <img src="img/vector_visual.jpg" class="card-img-top" alt="jpg">
-                <div class="card-body">
-                    <h4 class="card-title">Data visualisasi barang keluar</h4>
-                  
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-        <!-- Card 6 -->
-        <div class="col-lg-4 col-md-6 mb-3">
-            <div class="card small-card">
-                <img src="img/vector_visual.jpg" class="card-img-top" alt="jpg">
-                <div class="card-body">
-                    <h4 class="card-title">Peminjaman</h4>
-                   
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-                 
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <canvas id="lineChart"></canvas>
+                        <div class="row">
+                            <!-- Card 1 -->
+                            <div class="col-lg-4 col-md-6 mb-3">
+                                <div class="card small-card">
+                                    <img src="img/vector_visual.jpg" class="card-img-top" alt="jpg">
+                                    <div class="card-body">
+                                        <h4 class="card-title">Data visualisasi guru</h4>
+                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Card 2 -->
+                            <div class="col-lg-4 col-md-6 mb-3">
+                                <div class="card small-card">
+                                    <img src="img/vector_visual.jpg" class="card-img-top" alt="jpg">
+                                    <div class="card-body">
+                                        <h4 class="card-title">Data visualisasi siswa</h4>
+                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Card 3 -->
+                            <div class="col-lg-4 col-md-6 mb-3">
+                                <div class="card small-card">
+                                    <img src="img/vector_visual.jpg" class="card-img-top" alt="jpg">
+                                    <div class="card-body">
+                                        <h4 class="card-title">Data visualisasi barang</h4>
+                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <!-- Card 4 -->
+                            <div class="col-lg-4 col-md-6 mb-3">
+                                <div class="card small-card">
+                                    <img src="img/barang_masuk.png" class="card-img-top" alt="jpg">
+                                    <div class="card-body">
+                                        <h4 class="card-title">Data visualisasi barang masuk</h4>
+                                        <a href="#" id="showGrafikBarangMasuk" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#grafikModal">Tampilkan Grafik</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Card 5 -->
+                            <div class="col-lg-4 col-md-6 mb-3">
+                                <div class="card small-card">
+                                    <img src="img/vector_visual.jpg" class="card-img-top" alt="jpg">
+                                    <div class="card-body">
+                                        <h4 class="card-title">Data visualisasi barang keluar</h4>
+                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Card 6 -->
+                            <div class="col-lg-4 col-md-6 mb-3">
+                                <div class="card small-card">
+                                    <img src="img/vector_visual.jpg" class="card-img-top" alt="jpg">
+                                    <div class="card-body">
+                                        <h4 class="card-title">Peminjaman</h4>
+                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-
                 </div>
             </div>
+        </div>
+    </div>
 
+    <!-- Modal -->
+    <div class="modal fade" id="grafikModal" tabindex="-1" aria-labelledby="grafikModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="grafikModalLabel">Grafik Barang Masuk</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <canvas id="lineChart"></canvas>
+                </div>
+            </div>
+        </div>
+    </div>
 
+    <!-- Chart.js Library -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script>
+        document.addEventListener("DOMContentLoaded", function () {
+            // Inisialisasi grafik saat modal ditampilkan
+            const grafikModal = document.getElementById("grafikModal");
+            grafikModal.addEventListener("shown.bs.modal", function () {
+                const ctx = document.getElementById("lineChart").getContext("2d");
+                new Chart(ctx, {
+                    type: 'line',
+                    data: {
+                        labels: ['Januari', 'Februari', 'Maret', 'April', 'Mei'],
+                        datasets: [{
+                            label: 'Barang Masuk',
+                            data: [10, 20, 15, 25, 30],
+                            borderColor: 'rgba(75, 192, 192, 1)',
+                            borderWidth: 2,
+                            fill: false
+                        }]
+                    },
+                    options: {
+                        responsive: true,
+                        plugins: {
+                            legend: {
+                                position: 'top',
+                            },
+                            tooltip: {
+                                enabled: true,
+                            }
+                        },
+                        scales: {
+                            x: {
+                                title: {
+                                    display: true,
+                                    text: 'Bulan'
+                                }
+                            },
+                            y: {
+                                title: {
+                                    display: true,
+                                    text: 'Jumlah Barang'
+                                }
+                            }
+                        }
+                    }
+                });
+            });
+        });
+    </script>
 
 </body>
 
