8b6cb890f18d9ac375bee02258d294d7dc8beb42
riziqhilmi <riziqhilmi27@gmail.com>
2024-12-01 16:26:15 +0700

    Kelas Siswa

--- fitur/tambah_siswa.php
+++ fitur/tambah_siswa.php
@@ -11,14 +11,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $nama = $_POST['nama'];
     $nis = $_POST['nis'];
     $nisn = $_POST['nisn'];
+    $kelas = $_POST['kelas'];
     $jenis_kelamin = $_POST['jenis_kelamin']; 
     $tempat_lahir = $_POST['tempat_lahir'];
     $tgl_lahir = $_POST['tgl_lahir'];
     $agama = $_POST['agama'];
     $alamat = $_POST['alamat'];
 
-    $query = "INSERT INTO siswa (nama, NIS, NISN, jenis_kelamin, tempat_lahir, tgl_lahir, agama, alamat) 
-              VALUES ('$nama', '$nis', '$nisn', '$jenis_kelamin', '$tempat_lahir', '$tgl_lahir', '$agama', '$alamat')";
+    $query = "INSERT INTO siswa (nama, NIS, NISN, kelas, jenis_kelamin, tempat_lahir, tgl_lahir, agama, alamat) 
+              VALUES ('$nama', '$nis', '$nisn', $kelas, '$jenis_kelamin', '$tempat_lahir', '$tgl_lahir', '$agama', '$alamat')";
 
     if (mysqli_query($koneksi, $query)) {
         echo "<script>alert('Data siswa berhasil ditambahkan'); window.location='../data_siswa.php';</script>";
